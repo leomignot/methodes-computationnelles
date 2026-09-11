aa637cef769697eeb89dfedd37e08ace4da62aa5
leomignot <77462218+leomignot@users.noreply.github.com>
2026-09-11 10:03:23 +0200

    update demos

--- scripts/demo-topic-modeling.py
+++ scripts/demo-topic-modeling.py
@@ -3,20 +3,30 @@
 # # %pip install bertopic
 
 # %%
+import os
+
+os.environ["NUMBA_THREADING_LAYER"] = "workqueue"  # fix for kernel crash with UV
+
 # importer les bibliothèques nécessaires
 import pandas as pd
 from bertopic import BERTopic
 from sklearn.feature_extraction.text import CountVectorizer
+from stopwordsiso import stopwords
 
 # préciser l'url et charger les données
-url = "https://raw.githubusercontent.com/cristobalmitchell/pokedex/refs/heads/main/data/pokemon_utf8.csv"
+# url = "https://raw.githubusercontent.com/cristobalmitchell/pokedex/refs/heads/main/data/pokemon_utf8.csv"
+url = "hf://datasets/regicid/LRFAF/corpus.csv"
 df = pd.read_csv(url)
+df = df.sample(1000, random_state=42)  # échantillonner pour accélérer le calcul
 
-# passer les descriptions comme docs
-docs = df["description"].tolist()
+# passer les lyrics comme docs
+docs = df["lyrics"].tolist()
+
+# VECTORIZER pour gestion stopwords
+french_stopwords = list(stopwords("fr"))
+vectorizer_model = CountVectorizer(stop_words=french_stopwords)
 
 # créer le modèle de topic modeling et l'appliquer (avec gestion stopwords)
-vectorizer_model = CountVectorizer(stop_words="english")
 topic_model = BERTopic(vectorizer_model=vectorizer_model)
 topics, probs = topic_model.fit_transform(docs)
 
@@ -29,5 +39,8 @@ topics, probs = topic_model.fit_transform(docs)
 # topic_model.get_document_info(docs)
 topic_model.visualize_documents(docs)
 
-# %% [markdown]
-# # BUG VERSION NUMPY ? CF RETROUVER COMMENT A DEBUG PRÉCÉDENT ENV
+# %%
+topic_model.visualize_hierarchy()
+
+
+# %%
